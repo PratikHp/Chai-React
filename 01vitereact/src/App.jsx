180bf8c400799d33170c2a3ccf5bb6fe01481063
PratikHp <pratikhanagandi3@gmail.com>
2023-09-22 21:29:14 +0530

    Basic Done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "chai aur Code"
 
   return (
     <>
     <Chai/>
-    <h1>Chai Aur Code</h1>
+    <h1>Chai Aur Code {username}</h1>
     <h1>Test Para</h1>
     </>
     
